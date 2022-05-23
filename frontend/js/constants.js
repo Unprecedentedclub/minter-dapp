4e1c1782f73670be8ba8622eb7e6e966b254baec
Unprecedentedclub <timepieces786@gmail.comm>
2022-05-23 17:12:06 +0530

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x1b7A6013B084c2EFA33d622675e23390a384DE02"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
