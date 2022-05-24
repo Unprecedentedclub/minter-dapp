bc97ba4cf12a80dfc3a265224a95e3cf41bb344f
Unprecedentedclub <timepieces786@gmail.comm>
2022-05-24 17:44:51 +0530

    update

--- backend/src/config.js
+++ backend/src/config.js
@@ -61,7 +61,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-05-31T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-05-23T17:30:48+05:30"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-05-25T17:30:48+05:30"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x3405A57ddDd400fa27672C784C90C651Bcdac3a6"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
