a3770328999f5e07fefa7745db9325d4052a09ae
Unprecedentedclub <timepieces786@gmail.comm>
2022-05-24 18:23:06 +0530

    update

--- backend/src/config.js
+++ backend/src/config.js
@@ -54,11 +54,11 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0x3405A57ddDd400fa27672C784C90C651Bcdac3a6';
 const TREASURY_ADDRESS = '0x3405A57ddDd400fa27672C784C90C651Bcdac3a6';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = .1; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = .01; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-05-31T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-05-23T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2022-05-23T17:30:48+05:30"; // Optional. Eg: 2022-02-08T11:30:48+00:00
