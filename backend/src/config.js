0f128093c7fd11caaf2fa329c35d5505ecc77d87
Unprecedentedclub <timepieces786@gmail.comm>
2022-06-06 16:27:29 +0530

    update

--- backend/src/config.js
+++ backend/src/config.js
@@ -69,7 +69,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "0x11245fA0f50427Ed2C9fdB8533b93e26047Eb08b"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x3405A57ddDd400fa27672C784C90C651Bcdac3a6"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
